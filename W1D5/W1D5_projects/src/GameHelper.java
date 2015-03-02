2217f4e0820e112ba8e0a0c81efe514b5dfacc7c
kent7777777 <coolkent7777777@gmail.com>
2015-03-01 23:12:33 -0600

    W1D5

---
+++ W1D5/W1D5_projects/src/GameHelper.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kevin
+ */
+import java.io.*;
+public class GameHelper {
+    public String getUserInput(String prompt) {
+        String inputLine = null;
+        System.out.print(prompt + "  ");
+        try {
+            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+            inputLine = is.readLine();
+            if (inputLine.length() == 0 ) return null;
+        } catch (IOException e) { 
+            System.out.println("IOExeption: " + e);
+        }
+        return inputLine;
+    }
+}
